e619c158d68476b2e6010c66ef9a8adfbe18e397
Chathuka Dilakshana <IT22091352@my.sliit.lk>
2024-12-12 15:23:19 +0530

    Implement bus details form and display component with styling

---
+++ frontend/src/Components/Admin/addBus.js
@@ -0,0 +1,24 @@
+const Unit = ({ imageUrl, bNumber, bRoute, bName, bModel, bCapacity }) => {
+    return (
+      <div className="unit__container">
+        <img
+          src={imageUrl}
+          alt='images'
+          style={{
+            width: "200px",
+            height: "200px",
+          }}
+        />
+        <h3>{bNumber}</h3>
+        <p>
+          <span>{bRoute}</span>
+          <span>{bName}</span>
+          <span>{bModel}</span>
+          <span>{bCapacity}</span>
+        </p>
+      </div>
+    );
+  };
+  
+  export default Unit;
+  
